bf65494d5892301a66721d3f5467f4e7173ff56c
natnaelshiferaw2123 <natnaelshiferaw2123@gmail.com>
2025-03-06 14:49:22 +0000

    add mongoDB connection

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,11 +1,14 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "../../../lib/db";
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
-  ]
-})
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+});
