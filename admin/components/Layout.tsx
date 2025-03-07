aa9cbebf70ca11cbb625a6870a4acaace90d6245
natnaelshiferaw2123 <natnaelshiferaw2123@gmail.com>
2025-03-07 12:01:01 +0000

    add a Layout component

--- admin/components/Layout.tsx
+++ admin/components/Layout.tsx
@@ -1,41 +1,57 @@
 import NavBar from "@/components/NavBar";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 interface LayoutProps {
-    children : ReactNode;
+  children: ReactNode;
 }
 
-export default function Layout({children} : LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   const { data: session } = useSession();
 
   return (
+    <>
     <div className="flex">
       <NavBar />
-      
-      {/** checks for user's session */}
-      {!session ? (
-        <div className="bg-gray-50 w-screen h-screen flex items-center justify-center mr-2">
-          <button
-            className="p-2 px-4 bg-gray-300 rounded-lg hover:cursor-pointer"
-            onClick={() => signIn("google")} // Sign in function
-          >
-            Login with Google
-          </button>
+
+      <div className="flex flex-col gap-2 w-full">
+        {/** checks for user's session */}
+        <div>
+          {!session ? (
+            <div className="bg-gray-50 w-screen h-screen flex items-center justify-center mr-2">
+              <button
+                className="p-2 px-4 bg-gray-300 rounded-lg hover:cursor-pointer"
+                onClick={() => signIn("google")} // Sign in function
+              >
+                Login with Google
+              </button>
+            </div>
+          ) : (
+            <div className="flex bg-slate-100 justify-between m-2 ml-0 p-3 rounded-md">
+              <p>Welcome, <strong>{session.user?.name ?? "Unknown user"}</strong></p>
+              <div className="flex gap-2">
+                <button
+                  className="p-1 px-2 text-sm bg-red-500 text-white rounded-lg hover:cursor-pointer"
+                  onClick={() => signOut()} // Sign Out function
+                >
+                  LogOut
+                </button>
+                <img
+                    src={session.user?.image || ""}
+                    alt="User profile"
+                    className="w-10 h-10 rounded-full border border-gray-300" 
+                  />
+              </div>
+
+            </div>
+          )}
         </div>
-      ) : (
-        <div className="text-center mr-2 ml-2 mt-4">
-          Signed in as {session.user?.email ?? "Unknown user"}
-          <button
-            className="m-4 p-2 px-4 bg-gray-300 rounded-lg hover:cursor-pointer"
-            onClick={() => signOut()} // Sign Out function
-          >
-            LogOut
-          </button>
-          {children}
-        </div>
-      )}
+
+        {children}
+      </div>
 
     </div>
+    </>
   );
 }
